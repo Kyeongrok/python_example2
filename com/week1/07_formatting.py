6916405541f113578f43df979b50254569039342
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-04-07 12:04:59 +0900

    설명추가

--- com/week1/07_formatting.py
+++ com/week1/07_formatting.py
@@ -1,4 +1,4 @@
 print('greet:{} name:{}'.format('hello', 'kyeongrok'))
 result = '{} {}'.format('hello', 'kyeongrok2')
 print(result)
-print(type(result))
+print(type(result)) # format을 쓰면 string형이 된다.
