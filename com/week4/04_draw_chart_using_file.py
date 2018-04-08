262724b617bb91271599eb36f63254409bde12be
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-04-08 12:27:05 +0900

    np + matplot

--- com/week4/04_draw_chart_using_file.py
+++ com/week4/04_draw_chart_using_file.py
@@ -1,8 +1,18 @@
-f1 = open("./a.txt", mode='r')
+import matplotlib.pylab as plt
+import numpy as np
+
+f1 = open("./c.txt", mode='r')
 lines = f1.readlines()
 
 datas = []
 for line in lines:
     datas.append(line.replace("\n", ""))
 
-print(datas)
+x = np.arange(1, 1001, 1)
+y = np.array(datas)
+
+
+print(len(x))
+print(len(y))
+plt.plot(x, y)
+plt.show()
