5cac11483e075f48c94cf2185b3f83e5ae221920
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-04-08 11:59:31 +0900

    draw_chart_using_file

--- com/week4/01_save_to_file.py
+++ com/week4/01_save_to_file.py
@@ -1,10 +1,15 @@
+import numpy as np
+
+result = np.arange(10)
+
 f1 = open("./a.txt", mode='w+')
 f1.write("hello\n")
 f1.write("nello\n")
 f1.close()
 
-numbers = [1, 2]
+numbers = np.arange(1, 1000, 10)
 f2 = open("./b.txt", mode='w+')
 for number in numbers:
     f2.write(str(number) + "\n")
 f2.close()
+
